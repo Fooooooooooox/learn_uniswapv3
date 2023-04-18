fc27670630e27914244191a2b1d352586cccd471
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2023-04-18 12:26:11 +0800

    active adjustment strategy

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,4 @@
+import math
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 '''Utils'''
 '''
@@ -23,3 +24,24 @@ class utils:
     def price2RealPrice(self, price, decimal0, decimal1):
         realPrice = price * (10 ** (decimal0 - decimal1))
         return realPrice
+
+    def realPrice2Price(self, realPrice, decimal0, decimal1):
+        price = realPrice / (decimal0 - decimal1)
+        return price
+
+    def price2TickIndex(self, price):
+        index = math.log(price, 1.0001)
+        return index
+
+    def nearestTick(self, currentTick, tickSpacing):
+        modulus = currentTick // tickSpacing
+        remainder = currentTick % tickSpacing
+        nearestTick = modulus*tickSpacing if remainder < (tickSpacing/2) else (modulus+1)*tickSpacing
+        return nearestTick
+
+    # price 是未经过decimal转换的，正常形式的价格（可以用tickIndex2Price计算出来）
+    def calculateLiquidity(self, token0_amount, token1_amount, price_low, price_upper):
+        l1 = (token0_amount * (price_low ** 0.5) * (price_upper ** 0.5)) / ((price_upper ** 0.5) - (price_low ** 0.5))
+        l2 = token1_amount / ((price_upper ** 0.5) - (price_low ** 0.5))
+        l = min(l1, l2)
+        return l
